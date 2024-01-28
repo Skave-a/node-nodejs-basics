b337f73ad997a495c348787e9bc3c5001c0869a6
Skave-a <arrowtata@gmail.com>
2024-01-28 16:13:26 +0300

    feat: add env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const envVariables = [];
+
+  // process.env представляет собой объект, который содержит переменные окружения операционной системы
+  // Объект process.env предоставляет доступ в виде свойств объекта. Ключами объекта являются имена переменных окружения, а значениями - соответствующие значения.
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.startsWith(prefix)) {
+      envVariables.push(`${key}=${value}`);
+    }
+  }
+
+  const formattedEnvVariables = envVariables.join('; ');
+  console.log(formattedEnvVariables);
 };
 
 parseEnv();

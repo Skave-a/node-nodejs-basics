4fc252e63e9c22f9150505fd209c57bcddac3f66
Skave-a <arrowtata@gmail.com>
2024-01-28 17:29:38 +0300

    feat: add compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import zlib from 'zlib';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream, createWriteStream } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const filePathToGz = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+
+  const readStream = createReadStream(filePath);
+  const writeStream = createWriteStream(filePathToGz);
+
+  readStream.pipe(gzip).pipe(writeStream);
+
+  return new Promise((resolve, reject) => {
+    writeStream.on('finish', resolve);
+    writeStream.on('error', reject);
+  });
 };
 
 await compress();

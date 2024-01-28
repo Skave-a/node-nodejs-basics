3a6e3bc851d10620eaca989ef3091c3f3b446365
Skave-a <arrowtata@gmail.com>
2024-01-28 15:56:34 +0300

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
-const list = async () => {
-    // Write your code here 
-};
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-await list();
+const list = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const folderPath = path.join(__dirname, 'files');
+  
+    try {
+      await fs.promises.access(folderPath);
+      const fileNames = await fs.promises.readdir(folderPath);
+      console.log(fileNames);
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+      } else {
+        throw error;
+      }
+    }
+  };
+  
+  await list();
+  

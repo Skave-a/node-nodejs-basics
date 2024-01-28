6c64566d44a99adc11a7030a24ac94db63c2899d
Skave-a <arrowtata@gmail.com>
2024-01-28 14:17:52 +0300

    feat: add create fn

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = './files/fresh.txt';
+  const fileContent = 'I am fresh and young';
+
+  try {
+    // access - проверяет доступность файла или папки
+    await fs.promises.access(filePath);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    // ошибка ENOENT - файл не существует
+    if (error.code === 'ENOENT') {
+      await fs.promises.writeFile(filePath, fileContent);
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();

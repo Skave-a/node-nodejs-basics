a74d6f1fa743c97f667c41d350f62b032900fd7a
Skave-a <arrowtata@gmail.com>
2024-01-28 15:21:34 +0300

    feat: add copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,41 @@
-const copy = async () => {
-    // Write your code here 
-};
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+async function copy() {
+  // fileURLToPath(import.meta.url) используется для получения пути к текущему файлу
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const srcPath = path.join(__dirname, 'files');
+  const destPath = path.join(__dirname, 'files_copy');
+
+  try {
+    //  fs.stat возвращает объект, который содержит информацию о файле или папке, такую как размер, дату создания, дату изменения, права доступа и другие сведения.
+    await fs.stat(srcPath);
+    await fs.stat(destPath);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.mkdir(destPath);
+      const files = await fs.readdir(srcPath);
+
+      for (const file of files) {
+        const srcFilePath = path.join(srcPath, file);
+        const destFilePath = path.join(destPath, file);
+        const fileStats = await fs.stat(srcFilePath);
+
+        // является ли файл или папка директорией (папкой).
+        if (fileStats.isDirectory()) {
+          await copy(srcFilePath, destFilePath);
+        } else {
+          await fs.copyFile(srcFilePath, destFilePath);
+        }
+      }
+    } else {
+      throw error;
+    }
+  }
+}
+
 
 await copy();

4a38b0e120b44210c6fbf8a9ef4fb1bc5b5faf6e
Skave-a <arrowtata@gmail.com>
2024-01-28 17:13:32 +0300

    feat: add wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
-const performCalculations = async () => {
-    // Write your code here
+import os from 'os';
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const workerPath = path.resolve(__dirname, 'worker.js');
+
+const createWorkers = async () => {
+  const numCores = os.cpus().length;
+  const workers = [];
+
+  for (let i = 0; i < numCores; i++) {
+    const worker = new Worker(workerPath);
+    workers.push(worker);
+
+    const data = {
+      n: 10 + i
+    };
+
+    worker.postMessage({ data });
+  }
+
+  const results = await Promise.all(
+    workers.map(worker => {
+      return new Promise((resolve, reject) => {
+        worker.on('message', result => {
+          resolve(result);
+        });
+
+        worker.on('error', error => {
+          reject(error);
+        });
+      });
+    })
+  );
+
+  return results;
 };
 
-await performCalculations();
+const performCalculations = async () => {
+  const results = await createWorkers();
+  console.log(results);
+};
+
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,19 @@
-// n should be received from main thread
+import { parentPort } from 'worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const sendResult = (result) => {
+  parentPort.postMessage(result);
 };
 
-sendResult();
+parentPort.on('message', (event) => {
+  const data = event.data;
+  const n = data.n;
+
+  try {
+    const result = nthFibonacci(n);
+    sendResult({ status: 'resolved', data: result });
+  } catch (error) {
+    sendResult({ status: 'error', data: null });
+  }
+});

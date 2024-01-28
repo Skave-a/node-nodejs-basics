06dbac18f2e6c02437db76e94e8a51db0ad4a05d
Skave-a <arrowtata@gmail.com>
2024-01-28 16:55:39 +0300

    feat: add calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,32 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+  const hash = createHash('sha256');
+
+  const stream = createReadStream(filePath);
+
+  return new Promise((resolve, reject) => {
+    stream.on('data', (data) => {
+      hash.update(data);
+    });
+
+    stream.on('end', () => {
+      const hexHash = hash.digest('hex');
+      console.log(hexHash);
+      resolve();
+    });
+
+    stream.on('error', (err) => {
+      reject(err);
+    });
+  });
 };
 
 await calculateHash();

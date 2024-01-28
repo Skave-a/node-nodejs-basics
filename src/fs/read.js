840371bc7cc5ed610a337fc51a4600c23f46d783
Skave-a <arrowtata@gmail.com>
2024-01-28 16:00:51 +0300

    feat: add read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
-const read = async () => {
-    // Write your code here 
-};
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-await read();
+const read = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  
+    try {
+      await fs.promises.access(filePath);
+      const fileContent = await fs.promises.readFile(filePath, 'utf-8');
+      console.log(fileContent);
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        throw new Error('FS operation failed');
+      } else {
+        throw error;
+      }
+    }
+  };
+  
+  await read();
+  

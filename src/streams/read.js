df3c7ca3776dc94b090d237ade89f680db51c1da
Skave-a <arrowtata@gmail.com>
2024-01-29 20:53:50 +0300

    feat: add read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    const readableStream = fs.createReadStream(filePath);
+    readableStream.pipe(process.stdout);
 };
 
 await read();

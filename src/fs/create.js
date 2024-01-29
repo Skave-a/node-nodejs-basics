19e8f2ac1d5a9da32cf9f21b6355a3cbf14983d5
Skave-a <arrowtata@gmail.com>
2024-01-29 20:53:07 +0300

    fix: create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,20 @@
-import fs from 'fs';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const create = async () => {
-  const filePath = './files/fresh.txt';
-  const fileContent = 'I am fresh and young';
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+  const fileContent = "I am fresh and young";
 
   try {
     // access - проверяет доступность файла или папки
     await fs.promises.access(filePath);
-    throw new Error('FS operation failed');
+    throw new Error("FS operation failed");
   } catch (error) {
     // ошибка ENOENT - файл не существует
-    if (error.code === 'ENOENT') {
+    if (error.code === "ENOENT") {
       await fs.promises.writeFile(filePath, fileContent);
     } else {
       throw error;

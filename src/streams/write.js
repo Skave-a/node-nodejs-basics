63a502bad7b536e7598fe9babd146b4a96deaf6d
Skave-a <arrowtata@gmail.com>
2024-01-29 20:54:23 +0300

    feat: add write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,13 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, "files", "fileToWrite.txt");
+
+  process.stdin.pipe(fs.createWriteStream(filePath));
 };
 
 await write();
